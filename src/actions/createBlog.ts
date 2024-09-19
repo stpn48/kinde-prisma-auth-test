a056cfbb512e58409e55ec284cf149f5919f9a5b
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-19 14:35:37 +0200

    feat: server action to create blog

--- src/actions/createBlog.ts
+++ src/actions/createBlog.ts
@@ -4,7 +4,7 @@ import prisma from "@/lib/prisma";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { revalidatePath } from "next/cache";
 
-export async function createBlog(formData: FormData) {
+export async function createBlog(formData: FormData){
   const { isAuthenticated, getUser } = getKindeServerSession();
 
   if (!isAuthenticated()) {
@@ -13,13 +13,22 @@ export async function createBlog(formData: FormData) {
 
   const user = await getUser();
 
+  const title = formData.get("title") as string;
+  const content = formData.get("content") as string;
+
+  if (!title || !content) {
+    throw new Error("Title and content are required");
+  }
+
   await prisma.blog.create({
     data: {
       authorId: user.id,
-      title: formData.get("title") as string,
-      content: formData.get("content") as string,
+      title: title,
+      content: content,
     },
   });
 
   revalidatePath("/dashboard");
+
+  return { msg: "Blog created successfully" }
 }
