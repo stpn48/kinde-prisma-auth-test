5c170e199750ee18b48ca89536b05fbb8cb8728c
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-19 22:56:48 +0200

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 // Create a single instance of Prisma Client
 const prisma = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = prisma; // Keep the Prisma Client instance in global scope for non-production environments
 }
 
