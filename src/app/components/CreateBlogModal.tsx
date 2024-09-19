56d9ba0c31e5e9f1b6511c000205e4e79513ef97
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-19 22:57:27 +0200

    Update CreateBlogModal.tsx

--- src/app/components/CreateBlogModal.tsx
+++ src/app/components/CreateBlogModal.tsx
@@ -29,7 +29,7 @@ export function CreateBlogModal() {
         setCreateBlogModalOpened(false);
       });
     },
-    [setCreateBlogModalOpened]
+    [setCreateBlogModalOpened],
   );
 
   if (!createBlogModalOpened) {
@@ -38,32 +38,36 @@ export function CreateBlogModal() {
 
   return (
     <div
-      className="fixed font-geist-sans inset-0 w-screen h-screen flex items-center justify-center"
+      className="fixed inset-0 flex h-screen w-screen items-center justify-center font-geist-sans"
       onClick={isPending ? undefined : () => setCreateBlogModalOpened(false)}
     >
       <form
-        className="flex w-[500px] flex-col gap-2 py-4 px-6 rounded-lg shadow-md border modal-border-color modal-bg-color"
+        className="modal-border-color modal-bg-color flex w-[500px] flex-col gap-2 rounded-lg border px-6 py-4 shadow-md"
         onClick={(e) => e.stopPropagation()}
         action={(formData) => handleSubmit(formData)}
       >
-        <h1 className="text-white text-xl my-4 font-bold w-full flex justify-center">
+        <h1 className="my-4 flex w-full justify-center text-xl font-bold text-white">
           Create a blog
         </h1>
 
         <Input type="text" placeholder="Title" name="title" />
         <textarea
-          className="rounded-md text-sm text-white shadow-md p-2 modal-border-color border bg-[#0f0f0f] w-full h-[400px] resize-none"
+          className="modal-border-color h-[400px] w-full resize-none rounded-md border bg-[#0f0f0f] p-2 text-sm text-white shadow-md"
           placeholder="Content"
           name="content"
         />
-        {errorMsg && <p className="text-red-600 w-full flex justify-center text-sm">{errorMsg}</p>}
-        <div className="flex gap-2 justify-end w-full mt-4">
+        {errorMsg && (
+          <p className="flex w-full justify-center text-sm text-red-600">
+            {errorMsg}
+          </p>
+        )}
+        <div className="mt-4 flex w-full justify-end gap-2">
           <Button type="submit" disabled={isPending}>
             {isPending ? "Loading..." : "Create"}
           </Button>
           <Button
             disabled={isPending}
-            className="bg-[#313131] hover:bg-[#272727] text-white "
+            className="bg-[#313131] text-white hover:bg-[#272727]"
             onClick={() => setCreateBlogModalOpened(false)}
           >
             Cancel
