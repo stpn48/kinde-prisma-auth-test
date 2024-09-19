3bd5b59a99cabfe51ce49ad19f62d49fd1d3827b
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-19 14:36:54 +0200

    Create ReactQueryClientProvider.tsx

---
+++ src/app/components/ReactQueryClientProvider.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const queryClient = new QueryClient();
+
+export function ReactQueryClientProvider({ children }: Props) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools buttonPosition="bottom-left" initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
