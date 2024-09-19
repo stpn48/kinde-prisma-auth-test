9acdd5ea4099f49bb22f5ba0aee19606dd491ed8
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-19 22:57:10 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,17 @@
-import {
-  getKindeServerSession,
-  LoginLink,
-  RegisterLink,
-} from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/dist/server/api-utils";
-import Image from "next/image";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export default function Home() {
   return (
-    <div className="w-screen flex flex-col items-center justify-center p-4 text-white bg-[#131313] font-geist-sans min-h-screen">
-      <h1 className="font-bold text-3xl">Login with Kinde</h1>
-      <div className="flex gap-2 mt-10">
-        <LoginLink className="bg-white text-black px-2 py-1 rounded-sm">
+    <div className="flex min-h-screen w-screen flex-col items-center justify-center bg-[#131313] p-4 font-geist-sans text-white">
+      <h1 className="text-3xl font-bold">Continue with Kinde</h1>
+      <div className="mt-10 flex gap-2">
+        <LoginLink className="rounded-sm bg-white px-2 py-1 text-black">
           Login
         </LoginLink>
-        <RegisterLink className="bg-white text-black px-2 py-1 rounded-sm">
+        <RegisterLink className="rounded-sm bg-white px-2 py-1 text-black">
           Sign up
         </RegisterLink>
       </div>
     </div>
   );
 }
-
