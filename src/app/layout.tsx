55ba1c071a83fd2e96e44fadd9be73836b2fa634
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-19 14:36:12 +0200

    feat: add react query client provider custom client wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ModalVisibilityHandler } from "./components/ModalVisibilityHandler";
+import { ReactQueryClientProvider } from "./components/ReactQueryClientProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,8 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <ModalVisibilityHandler />
+        <ReactQueryClientProvider>
+          {children}
+          <ModalVisibilityHandler />
+        </ReactQueryClientProvider>
       </body>
     </html>
   );
