6826dbff3b8a0d25ddf2e22025fad3a3630ea938
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-20 23:47:23 +0200

    Create CreateBlogModal.tsx

---
+++ src/app/dashboard/components/CreateBlogModal.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import React, { useCallback, useTransition } from "react";
+
+import { useErrorMsg } from "@/hooks/useErrorMsg";
+import { createBlog } from "@/actions/createBlog";
+import { Input } from "@/app/components/Input";
+import { Button } from "@/app/components/Button";
+import { useModalVisibilityStore } from "@/store/useModalVisibilityStore";
+
+export function CreateBlogModal() {
+  const { createBlogModalOpened, setCreateBlogModalOpened } = useModalVisibilityStore();
+
+  const [isCreatingBlog, startTransition] = useTransition();
+  const { errorMsg, setErrorMsg } = useErrorMsg();
+
+  const handleSubmit = useCallback(
+    (formData: FormData) => {
+      const title = formData.get("title") as string;
+      const content = formData.get("content") as string;
+
+      if (!title || !content) {
+        setErrorMsg("Title and content are required");
+        return;
+      }
+
+      startTransition(async () => {
+        await createBlog(formData);
+        setCreateBlogModalOpened(false);
+      });
+    },
+    [setCreateBlogModalOpened],
+  );
+
+  if (!createBlogModalOpened) {
+    return null;
+  }
+
+  return (
+    <div
+      className="fixed inset-0 flex h-screen w-screen items-center justify-center font-geist-sans"
+      onClick={isCreatingBlog ? undefined : () => setCreateBlogModalOpened(false)}
+    >
+      <form
+        className="modal-border-color modal-bg-color flex w-[500px] flex-col gap-2 rounded-lg border px-6 py-4 shadow-md"
+        onClick={(e) => e.stopPropagation()}
+        action={(formData) => handleSubmit(formData)}
+      >
+        <h1 className="my-4 flex w-full justify-center text-xl font-bold text-white">Create a blog</h1>
+
+        <Input type="text" placeholder="Title" name="title" />
+        <textarea
+          className="modal-border-color h-[400px] w-full resize-none rounded-md border bg-[#0f0f0f] p-2 text-sm text-white shadow-md"
+          placeholder="Content"
+          name="content"
+        />
+        {errorMsg && <p className="flex w-full justify-center text-sm text-red-600">{errorMsg}</p>}
+        <div className="mt-4 flex w-full justify-end gap-2">
+          <Button disabled={isCreatingBlog} variant="secondary" onClick={() => setCreateBlogModalOpened(false)}>
+            Cancel
+          </Button>
+          <Button type="submit" disabled={isCreatingBlog} isLoading={isCreatingBlog}>
+            Create
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
