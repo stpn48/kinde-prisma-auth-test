640c7b800a0ccf34328ca1fefe1e7d38ba5ed473
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-20 14:56:30 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ModalVisibilityHandler } from "./components/ModalVisibilityHandler";
 import { ReactQueryClientProvider } from "./components/ReactQueryClientProvider";
+import { CreateBlogModal } from "./components/CreateBlogModal/CreateBlogModal";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,12 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ReactQueryClientProvider>
           {children}
-          <ModalVisibilityHandler />
+          <CreateBlogModal />
         </ReactQueryClientProvider>
       </body>
     </html>
