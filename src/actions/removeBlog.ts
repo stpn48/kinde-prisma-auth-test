d8c2e13419f20cd24412843c1e05cb60c9d4581d
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-21 21:42:50 +0200

    Update removeBlog.ts

--- src/actions/removeBlog.ts
+++ src/actions/removeBlog.ts
@@ -5,7 +5,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { revalidatePath } from "next/cache";
 
 export async function removeBlog(blogId: string) {
-  const { isAuthenticated, getUser } = getKindeServerSession();
+  const { isAuthenticated, getUser, getPermissions } = getKindeServerSession();
 
   const isLoggedIn = await isAuthenticated();
 
@@ -13,14 +13,25 @@ export async function removeBlog(blogId: string) {
     throw new Error("You must be logged in to remove a blog");
   }
 
-  const user = await getUser();
+  const kindePermissions = await getPermissions();
+  const isAdmin = kindePermissions?.permissions?.includes("remove:blog");
 
-  await prisma.blog.delete({
-    where: {
-      id: blogId,
-      authorId: user.id,
-    },
-  });
+  if (isAdmin) {
+    await prisma.blog.delete({
+      where: {
+        id: blogId,
+      },
+    });
+  } else {
+    const user = await getUser();
+
+    await prisma.blog.delete({
+      where: {
+        id: blogId,
+        authorId: user.id,
+      },
+    });
+  }
 
   revalidatePath("/");
 
