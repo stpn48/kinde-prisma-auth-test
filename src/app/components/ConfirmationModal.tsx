21a7e32c313b512af01cf598bb6e2927244716aa
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-21 21:42:55 +0200

    Update ConfirmationModal.tsx

--- src/app/components/ConfirmationModal.tsx
+++ src/app/components/ConfirmationModal.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useCallback } from "react";
 import { Button } from "./Button";
 import { ModalBackDrop } from "./ModalBackDrop";
 import ModalBody from "./ModalBody";
@@ -12,12 +14,17 @@ type Props = {
 };
 
 export function ConfirmationModal({ closeModal, confirmAction, message, cancelButtonText, confirmButtonText }: Props) {
+  const confirmButtonClick = useCallback(() => {
+    confirmAction();
+    closeModal();
+  }, []);
+
   return (
     <ModalBackDrop className="h-full w-full" onClick={closeModal}>
       <ModalBody className="flex h-fit w-fit flex-col gap-4">
         <h1 className="text-center">{message}</h1>
         <div className="flex w-full justify-center gap-2">
-          <Button onClick={confirmAction} variant="secondary">
+          <Button onClick={confirmButtonClick} variant="secondary">
             {confirmButtonText}
           </Button>
           <Button onClick={closeModal}>{cancelButtonText}</Button>
