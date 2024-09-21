3586003b20a54090bdc11856d31843e2bbe15b9d
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-21 21:43:13 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Button } from "./components/Button";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-screen flex-col items-center justify-center bg-[#131313] p-4 font-geist-sans text-white">
-      <h1 className="text-3xl font-bold">Continue with Kinde</h1>
-      <div className="mt-10 flex gap-2">
-        <LoginLink className="rounded-sm bg-white px-2 py-1 text-black">
-          Login
-        </LoginLink>
-        <RegisterLink className="rounded-sm bg-white px-2 py-1 text-black">
-          Sign up
-        </RegisterLink>
+    <div className="flex min-h-screen w-screen flex-col items-center justify-center p-4 font-geist-sans text-white">
+      <h1 className="text-4xl font-bold">Continue with Kinde</h1>
+      <div className="mt-10 flex gap-4">
+        <Button className="px-4 text-lg" variant="secondary">
+          <LoginLink>Login</LoginLink>
+        </Button>
+        <Button className="px-4 text-lg">
+          <RegisterLink>Sign up</RegisterLink>
+        </Button>
       </div>
     </div>
   );
