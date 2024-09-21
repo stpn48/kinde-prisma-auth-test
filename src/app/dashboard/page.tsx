72f85c35152c46eaec19c4acec35ec4a4e06bc0b
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-21 21:44:32 +0200

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,14 @@
 import { getKindeServerSession, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
-import prisma from "@/lib/prisma";
 import { DashboardActionButtons } from "@/app/dashboard/components/DashboardActionButtons";
-import { BlogCard } from "./components/BlogCard";
-import { Blog } from "@prisma/client";
 import { BlogDetailsModal } from "./components/BlogDetailsModal/BlogDetailsModal";
 import { CreateBlogModal } from "./components/CreateBlogModal";
+import { Suspense } from "react";
+import { BlogList } from "./components/BlogList";
+import { BlogDetailsModalLoading } from "./components/BlogDetailsModal/BlogDetailsModalLoading";
 
-export default async function Dashboard({ searchParams }: { searchParams: { blogId?: string } }) {
-  const { isAuthenticated, getUser } = getKindeServerSession();
+export default async function Dashboard({ searchParams }: { searchParams: { blogId?: string; creatingBlog?: boolean } }) {
+  const { isAuthenticated } = getKindeServerSession();
 
   const isLoggedIn = await isAuthenticated();
 
@@ -16,33 +16,26 @@ export default async function Dashboard({ searchParams }: { searchParams: { blog
     redirect("/api/auth/login");
   }
 
-  const user = await getUser();
-
-  const userBlogs: Blog[] = await prisma.blog.findMany({
-    where: {
-      authorId: user.id,
-    },
-  });
-
   return (
-    <div className="min-h-screen w-screen bg-[#121212] p-4 font-geist-sans text-white">
+    <div className="min-h-screen w-screen p-4">
       <div className="flex justify-between">
         <h1 className="text-2xl font-bold">Your blogs</h1>
         <DashboardActionButtons />
       </div>
 
-      <div className="mt-10 flex w-fit flex-wrap gap-2">
-        {userBlogs.map((blog) => (
-          <BlogCard key={blog.id} blog={blog} />
-        ))}
-      </div>
+      <BlogList />
 
       <LogoutLink className="absolute bottom-2 right-2 rounded-sm border border-red-600 bg-inherit px-2 py-1 text-red-600">
         Logout
       </LogoutLink>
 
-      <CreateBlogModal />
-      <BlogDetailsModal searchParams={searchParams} />
+      {searchParams.creatingBlog && <CreateBlogModal />}
+
+      {searchParams.blogId && (
+        <Suspense key={Date.now()} fallback={<BlogDetailsModalLoading />}>
+          <BlogDetailsModal blogId={searchParams.blogId} />
+        </Suspense>
+      )}
     </div>
   );
 }
