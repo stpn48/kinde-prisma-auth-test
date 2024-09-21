6d64127460b0ca3857dedcd59e7941f7eceff871
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-21 21:43:57 +0200

    Update BlogDetailsModal.tsx

--- src/app/dashboard/components/BlogDetailsModal/BlogDetailsModal.tsx
+++ src/app/dashboard/components/BlogDetailsModal/BlogDetailsModal.tsx
@@ -2,19 +2,43 @@ import React from "react";
 import { Modal } from ".//Modal";
 import prisma from "@/lib/prisma";
 import { Blog } from "@prisma/client";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export async function BlogDetailsModal({ searchParams }: { searchParams: { blogId?: string } }) {
-  const blogId = searchParams.blogId;
+type Props = {
+  blogId: string;
+};
 
-  if (!blogId) {
-    return null;
+export async function BlogDetailsModal({ blogId }: Props) {
+  const { getUser, getPermissions } = getKindeServerSession();
+
+  const kindePermissions = await getPermissions();
+  const userPermissions = kindePermissions?.permissions || [];
+
+  let blogDetails: Blog;
+
+  //TODO: remove this
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  // if user is admin, they can view details for any blog.
+  if (userPermissions.includes("remove:blog")) {
+    blogDetails = await prisma.blog.findUnique({
+      where: {
+        id: blogId,
+      },
+    });
   }
 
-  const blogDetails: Blog = await prisma.blog.findUnique({
-    where: {
-      id: blogId,
-    },
-  });
+  // normal user can only view his own blog details.
+  else {
+    const user = await getUser();
+
+    blogDetails = await prisma.blog.findUnique({
+      where: {
+        authorId: user.id,
+        id: blogId,
+      },
+    });
+  }
 
   return <Modal blogDetails={blogDetails} />;
 }
