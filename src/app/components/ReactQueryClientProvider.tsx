aacd481e88e612f3327258ab2f7361640cfaaec4
stepan <133258815+stpn48@users.noreply.github.com>
2024-09-21 21:43:33 +0200

    refactor: remove RQ dev tools

--- src/app/components/ReactQueryClientProvider.tsx
+++ src/app/components/ReactQueryClientProvider.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 type Props = {
   children: React.ReactNode;
@@ -11,10 +10,5 @@ type Props = {
 const queryClient = new QueryClient();
 
 export function ReactQueryClientProvider({ children }: Props) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools buttonPosition="bottom-left" initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
